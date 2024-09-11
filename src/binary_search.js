1eae796af3359929a06fdf3b4c37cfc563a43ebb
diyora <diyoramekhmonova@gmail.com>
2024-09-11 14:56:29 +0800

    feat: binary search recursive approach

--- src/binary_search.js
+++ src/binary_search.js
@@ -16,4 +16,22 @@ function binarySearch(arr, target) {
   return -1;
 }
 
-console.log(binarySearch([2, 8, 1, 4, 5, 7, 3], 5));
+// console.log(binarySearch([2, 8, 1, 4, 5, 7, 3], 5));
+
+function binarySearchRecursive(arr, target, left, right) {
+  if (left > right) {
+    return -1;
+  }
+
+  let mid = Math.floor((left + right) / 2);
+
+  if (arr[mid] === target) {
+    return mid;
+  }
+  if (arr[mid] < target) {
+    return binarySearchRecursive(arr, target, mid + 1, right);
+  }
+  return binarySearchRecursive(arr, target, left, mid - 1);
+}
+
+// console.log(binarySearchRecursive([2, 8, 1, 4, 5, 7, 3], 5, 0, 7 - 1));
